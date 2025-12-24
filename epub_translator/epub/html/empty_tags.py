a661482c299c57386a1702fb15dd6cf7f7227f5e
TaoZeyu <i@taozeyu.com>
2025-12-24 09:31:49 +0800

    style: fix style for the epub folder

--- epub_translator/epub/html/empty_tags.py
+++ epub_translator/epub/html/empty_tags.py
@@ -3,21 +3,21 @@ import re
 # HTML 规定了一系列自闭标签，这些标签需要改成非自闭的，因为 EPub 格式不支持
 # https://www.tutorialspoint.com/which-html-tags-are-self-closing
 _EMPTY_TAGS = (
-  "br",
-  "hr",
-  "input",
-  "col",
-  "base",
-  "meta",
-  "area",
+    "br",
+    "hr",
+    "input",
+    "col",
+    "base",
+    "meta",
+    "area",
 )
 
-_EMPTY_TAG_PATTERN = re.compile(
-  r"<(" + "|".join(_EMPTY_TAGS) + r")(\s[^>]*?)\s*/?>"
-)
+_EMPTY_TAG_PATTERN = re.compile(r"<(" + "|".join(_EMPTY_TAGS) + r")(\s[^>]*?)\s*/?>")
+
 
 def to_html(content: str) -> str:
-  return re.sub(_EMPTY_TAG_PATTERN, lambda m: f"<{m.group(1)}{m.group(2)}>", content)
+    return re.sub(_EMPTY_TAG_PATTERN, lambda m: f"<{m.group(1)}{m.group(2)}>", content)
+
 
 def to_xml(content: str) -> str:
-  return re.sub(_EMPTY_TAG_PATTERN, lambda m: f"<{m.group(1)}{m.group(2)} />", content)
+    return re.sub(_EMPTY_TAG_PATTERN, lambda m: f"<{m.group(1)}{m.group(2)} />", content)
