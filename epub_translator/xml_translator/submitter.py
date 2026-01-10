139f58e9c55167219e7f1962a6280f11981deabc
TaoZeyu <i@taozeyu.com>
2026-01-10 11:54:14 +0800

    feat: add _nest_nodes

--- epub_translator/xml_translator/submitter.py
+++ epub_translator/xml_translator/submitter.py
@@ -1,8 +1,94 @@
+from enum import Enum, auto
+from typing import cast
 from xml.etree.ElementTree import Element
 
 from ..segment import TextSegment, combine_text_segments
+from ..utils import nest
 from ..xml import index_of_parent, iter_with_stack
 from .stream_mapper import InlineSegmentMapping
+
+
+class SubmitAction(Enum):
+    Replace = auto()
+    Append = auto()
+
+
+class _PeakNode:
+    def __init__(self, raw_element: Element, text_segments: list[TextSegment]) -> None:
+        pass
+
+
+class _PlatformNode:
+    def __init__(
+        self,
+        raw_element: Element,
+        items: list[tuple[list[TextSegment], "_PeakNode | _PlatformNode"]],
+        tail_text_segments: list[TextSegment],
+    ) -> None:
+        pass
+
+
+def _nest_nodes(mappings: list[InlineSegmentMapping]):
+    # 需要翻译的文字会被嵌套到两种不同的结构中。
+    # 最常见的的是 peak 结构，例如如下结构，没有任何子结构。可直接文本替换或追加。
+    # <div>Some text <b>bold text</b> more text.</div>
+    #
+    # 但是还有一种少见的 platform 结构，它内部被其他 peak/platform 切割。
+    #       <div>
+    #         Some text before.
+    #         <!-- 如下 peak 将它的阅读流切段 -->
+    #         <div>Paragraph 1.</div>
+    #         Some text in between.
+    #       </div>
+    # 如果直接对它进行替换或追加，读者阅读流会被破坏，从而读起来怪异。
+    # 正是因为这种结构的存在，必须还原成树型结构，然后用特殊的方式来处理 platform 结构。
+    #
+    # 总之，我们假设 95% 的阅读体验由 peak 提供，但为兼顾剩下的 platform 结构，故加此步骤。
+    grouped_text_segments = nest((id(e), text_segments) for e, text_segments in mappings)
+    id2children: dict[int, list[int | _PeakNode | _PlatformNode]] = dict(
+        (id, []) for id in grouped_text_segments.keys()
+    )
+    for group_id, group in grouped_text_segments.items():
+        text_segment = group[0][0]
+        # ignore the last one (it's self element)
+        for i in range(len(text_segment.parent_stack) - 2, -1, -1):
+            parent = text_segment.parent_stack[i]
+            children = id2children.get(id(parent), None)
+            if children is not None:
+                children.append(group_id)
+                break
+
+    nodes_buffer: dict[int, _PeakNode | _PlatformNode] = {}
+    while id2children:
+        for group_id, children in list(id2children.items()):
+            group = grouped_text_segments[group_id]
+            if not children:
+                id2children.pop(group_id)
+                nodes_buffer[group_id] = _PeakNode(
+                    raw_element=group[0][0].block_parent,
+                    text_segments=[t for ts in group for t in ts],
+                )
+            else:
+                progress_count: int = 0
+                for i in range(len(children)):
+                    child = children[i]
+                    if not isinstance(child, int):
+                        progress_count += 1
+                    else:
+                        child_node = nodes_buffer.get(child, None)
+                        if child_node is not None:
+                            children[i] = child_node
+                            progress_count += 1
+                if progress_count >= len(children):
+                    id2children.pop(group_id)
+                    nodes_buffer[group_id] = _create_platform_node(
+                        group=group,
+                        children=cast(list[_PeakNode | _PlatformNode], children),
+                    )
+
+
+def _create_platform_node(group: list[list[TextSegment]], children: list[_PeakNode | _PlatformNode]) -> _PlatformNode:
+    raise NotImplementedError()
 
 
 def submit_text_segments(element: Element, mappings: list[InlineSegmentMapping]) -> Element:
